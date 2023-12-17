ad57ec05fe39db78a2a9afb81e18de2114aad926
Nwamara Chijioke <Nwamara.chijioke@gmail.com>
2023-12-17 22:55:12 +0100

    Web app working commit

---
+++ Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FinancialCalculatorWebApp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <div class="row mt-4">
+             <section id="calculatorSection">
+                <h2>Financial Calculator</h2>
+        
+                <label for="ddlCalculation">Select Calculation:</label>
+                <asp:DropDownList ID="ddlCalculation" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCalculation_SelectedIndexChanged">
+                    <asp:ListItem Text="Select Calculation Type" Value="Select"></asp:ListItem>
+                    <asp:ListItem Text="Calculate Simple Interest" Value="SimpleInterest"></asp:ListItem>
+                    <asp:ListItem Text="Calculate Principal" Value="Principal"></asp:ListItem>
+                    <asp:ListItem Text="Calculate Rate" Value="Rate"></asp:ListItem>
+                    <asp:ListItem Text="Calculate Time" Value="Time"></asp:ListItem>
+                </asp:DropDownList>
+        
+                 <div id="divPrincipal" runat="server">
+                    <label for="txtPrincipal">Principal Amount:</label>
+                    <asp:TextBox ID="txtPrincipal" runat="server" CssClass="form-control" type="number"></asp:TextBox>
+                 </div>
+                 <div id="divSimpleInterest" runat="server">
+                    <label for="txtInterest">SimpleInterest:</label>
+                    <asp:TextBox ID="txtInterest" runat="server" CssClass="form-control" type="number"></asp:TextBox>
+                </div>
+
+                <div id="divRate" runat="server">
+                    <label for="txtRate">Rate:</label>
+                    <asp:TextBox ID="txtRate" runat="server" CssClass="form-control" type="number"></asp:TextBox>
+                </div>
+
+                <div id="divTime" runat="server">
+                    <label for="txtTime">Time(Days):</label>
+                    <asp:TextBox ID="txtTime" runat="server" CssClass="form-control" type="number"></asp:TextBox>
+                </div>
+
+                <asp:Button ID="btnCalculate" runat="server" CssClass="btn btn-secondary mt-4" Text="Calculate" OnClick="btnCalculate_Click" />
+        
+                <br />
+
+                <asp:Label ID="lblResult" runat="server" Visible="false" style="color: green;"></asp:Label>
+            </section>
+      </div>
+    </main>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinancialCalculatorWebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //divPrincipal.Visible = true;
+            //divRate.Visible = true;
+            //divTime.Visible = true;
+            //divSimpleInterest.Visible = false;
+        }
+
+        protected void ddlCalculation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCalculation = ddlCalculation.SelectedValue;
+
+            // Show/hide input fields based on the selected calculation
+            switch (selectedCalculation)
+            {
+                case "Principal":
+                    divPrincipal.Visible = false;
+                    divRate.Visible = true;
+                    divTime.Visible = true;
+                    divSimpleInterest.Visible = true;
+                    break;
+                case "Rate":
+                    divPrincipal.Visible = true;
+                    divRate.Visible = false;
+                    divTime.Visible = true;
+                    divSimpleInterest.Visible = true;
+                    break;
+                case "Time":
+                    divPrincipal.Visible = true;
+                    divRate.Visible = true;
+                    divTime.Visible = false;
+                    divSimpleInterest.Visible = true;
+                    break;
+                case "SimpleInterest":
+                    divPrincipal.Visible = true;
+                    divRate.Visible = true;
+                    divTime.Visible = true;
+                    divSimpleInterest.Visible = false;
+                    break;
+            }
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string selectedCalculation = ddlCalculation.SelectedValue;
+
+            // Perform calculation based on the selected type
+            string result = "";
+
+            switch (selectedCalculation)
+            {
+                case "Principal":
+                    result = CalculatePrincipal();
+                    break;
+                case "Rate":
+                    result = CalculateRate();
+                    break;
+                case "Time":
+                    result = CalculateTime();
+                    break;
+                case "SimpleInterest":
+                    result = CalculateSimpleInterest();
+                    break;
+            }
+
+            // Display the result
+            lblResult.Text = $"Result: {result}"; // Format as currency
+            lblResult.Visible = true;
+        }
+
+       
+        private string CalculatePrincipal()
+        {
+            // Implement the logic for calculating Principal
+            //instantiating our web service
+            //FinCalcReference.FinancialCalculatorWebServiceSoapClient client = new FinCalcReference.FinancialCalculatorWebServiceSoapClient();
+            LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient client = new LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient();
+            
+            // calling our web service for calculating principal
+            double rate = Convert.ToDouble(txtRate.Text);
+            double time = Convert.ToDouble(txtTime.Text);
+            double simpleInterest = Convert.ToDouble(txtInterest.Text);
+            var principal = client.CalculatePrincipal(simpleInterest, rate, time);
+
+            return principal;
+        }
+
+        private string CalculateRate()
+        {
+            // Implement the logic for calculating Rate
+
+            //instantiating our web service
+            //FinCalcReference.FinancialCalculatorWebServiceSoapClient client = new FinCalcReference.FinancialCalculatorWebServiceSoapClient();
+            LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient client = new LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient();
+            double principal = Convert.ToDouble(txtPrincipal.Text);
+            double time = Convert.ToDouble(txtTime.Text);
+            double simpleInterest = Convert.ToDouble(txtInterest.Text);
+
+            var rate = client.CalculateRate(principal, simpleInterest, time);
+            return rate;
+        }
+
+        private string CalculateTime()
+        {
+            // Implement the logic for calculating Time
+            //instantiating our web service
+            //FinCalcReference.FinancialCalculatorWebServiceSoapClient client = new FinCalcReference.FinancialCalculatorWebServiceSoapClient();
+            LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient client = new LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient();
+            double principal = Convert.ToDouble(txtPrincipal.Text);
+            double rate = Convert.ToDouble(txtRate.Text);
+            double simpleInterest = Convert.ToDouble(txtInterest.Text);
+
+            var time = client.CalculateTime(principal, simpleInterest, rate);
+            return time;
+        }
+
+        private string CalculateSimpleInterest()
+        {
+            // Implement the logic for calculating Simple Interest
+            //instantiating our web service
+            //FinCalcReference.FinancialCalculatorWebServiceSoapClient client = new FinCalcReference.FinancialCalculatorWebServiceSoapClient();
+            LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient client = new LiveFinCalcRef.FinancialCalculatorWebServiceSoapClient();
+
+            double principal = Convert.ToDouble(txtPrincipal.Text);
+            double rate = Convert.ToDouble(txtRate.Text);
+            double time = Convert.ToDouble(txtTime.Text);
+
+            var simpleInterest = client.CalculateSimpleInterest(principal, rate,time);
+            return simpleInterest;
+        }
+    }
+
+}
